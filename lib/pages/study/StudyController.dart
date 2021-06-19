ca20a9191cb2c5df97175b0f5c8bd776f1f5e9b0
unknown <rayhangamawanto@gmail.com>
2021-06-19 14:31:46 +0700

    fixing auth status

--- lib/pages/study/StudyController.dart
+++ lib/pages/study/StudyController.dart
@@ -1,14 +1,22 @@
 import 'package:get/get.dart';
 import 'package:work_test/services/services.dart';
+import 'package:work_test/pages/auth/AuthScreen.dart';
 
 class StudyController extends GetxController {
   var getToken = GetSharedPreferences.getToken(key: "token");
   List studies;
   bool isLoading = false;
+  var userId;
+
   @override
   void onInit() async {
     super.onInit();
     print("Study Controller Mounted");
+    var token = await getToken;
+    var user = await AuthServices.userVerify(token: token);
+    if (user == null) {
+      Get.off(() => AuthScreen());
+    }
     getAllStudies();
   }
 
